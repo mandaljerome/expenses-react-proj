b2ccbd83c80f2baa78077e49d601fe08c193d447
Jerome Mandal <mandaljerome@yahoo.com>
2023-03-23 10:42:03 +0800

    Commit 3.23.23

--- src/components/NewExpenses/NewExpenseForm.jsx
+++ src/components/NewExpenses/NewExpenseForm.jsx
@@ -1,28 +1,35 @@
 import './NewExpenseForm.scss'
+import { useState } from 'react'
 
 const NewExpenseForm = () => {
-
-    const formSubmit = (event) => {
-      event.preventDefault()
-      console.log('I was clicked!')
+    const [inputData, setInputData] = useState({title: '', amount: '', date: ''})
+    
+    const inputChange = (event) => {
+        setInputData(prev => {
+          return {
+            ...prev,
+            [event.target.name]: event.target.value
+          }
+        })
     }
-
+    
+    console.log(inputData)
 
     return (
         <div className='new-expense-form'>
-            <form action='submit' onSubmit={formSubmit}>
+            <form>
                 <div className='inputs'>
                     <div className='form-control'>
                         <label>Title</label>
-                        <input type='text' />
+                        <input type='text' name='title' value={inputData.title} onChange={inputChange}/>
                     </div>
                     <div className='form-control'>
                         <label>Amount</label>
-                        <input type='number' />
+                        <input type='number' name='amount' value={inputData.amount} onChange={inputChange}/>
                     </div>
                     <div className='form-control'>
                         <label>Date</label>
-                        <input type='date' min='2020-01-01' max='2025-12-31' />
+                        <input type='date' name='date' min='2020-01-01' value={inputData.date} max='2025-12-31' onChange={inputChange}/>
                     </div>
                 </div>
                 <button type='submit'>Add Expenses</button>
